694c5005f0cc19a074fbd0afea71b3597a0ecc79
David <davidlozano05@gmail.com>
2019-02-15 10:26:18 +0100

    Project shell

---
+++ trans-rest-api/src/main/java/transrestapi/App.java
@@ -0,0 +1,33 @@
+package transrestapi;
+
+import io.javalin.Javalin;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
+
+public final class App {
+
+    private App() {
+    }
+
+    private static final Javalin app = create();
+
+    public static void main(String[] args) {
+        start();
+    }
+
+    public static void start() {
+        app.start();
+    }
+
+    public static void stop() {
+        app.stop();
+    }
+
+    private static Javalin create() {
+        return Javalin.create()
+                .port(7070)
+                .routes(() -> {
+                    get("/health", ctx -> ctx.result("Running..."));
+                });
+    }
+}
